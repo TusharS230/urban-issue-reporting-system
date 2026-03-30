8f8475a99b61e7b0a9b6bbcbbb17f83752248b2e
TusharS230 <tusharsinghdec@gmail.com>
2026-03-30 15:22:00 +0530

    Debuged the issue of login and register

--- src/main/java/com/urbanissue/controller/UserController.java
+++ src/main/java/com/urbanissue/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.urbanissue.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import com.urbanissue.model.User;
 import com.urbanissue.service.UserService;
 import org.springframework.http.ResponseEntity;
